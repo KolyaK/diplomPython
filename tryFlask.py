4aa4630710558008e91c8c77a55ada0d957a88b4
KolyaK <llmon2535@gmail.com>
2019-05-08 18:31:02 +0300

    First step

---
+++ tryFlask.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@ app.route("/")
+def hello():
+    return "hello word"
+
+
+@ app.route("/hi")
+def hi():
+    user = {'nickname': 'Kolya'}  # выдуманный пользователь
+    return '''
+    <html>
+      <head>
+        <title>Home Page</title>
+      </head>
+      <body>
+        <h1>Hello, ''' + user['nickname'] + '''</h1>
+      </body>
+    </html>
+    '''
+
+
+@ app.route("/user/<name>")  # прием параметра через  url 
+def user(name):
+    return "<h1> Hello, {} </h1>".format(name)
+
+
+# if __name__ == '__main__':
+app.run()
