524b05bbdd2e2853a5d2bb2bb0db0a29c1216647
KolyaK <llmon2535@gmail.com>
2019-05-10 20:45:12 +0300

    First step

--- ipAddress.py
+++ ipAddress.py
@@ -1,22 +1,26 @@
 class test_ip:
     def __init__(self, ip):
         self.ip = ip
+        self.bin_ip = {}
+
 
     def split_ip(self):
         splited_ip = str(self.ip).split(".", 3)
         return splited_ip
 
     def ip_to_bin(self, splitted_ip):
-        bin_ip = ""
+
         i = 0
         for x in splitted_ip:
-            print("{:+b}".format(x))
+            self.bin_ip[i] = "{message:{fill}>{width}}".format(message=bin(int(x))[2:], fill="0", width=8)
             i += 1
-        return print(bin_ip)
+        return self.bin_ip
 
+    def __str__(self):
+        result = str(self.bin_ip[0] + "." + self.bin_ip[1] + "." + self.bin_ip[2] + "." + self.bin_ip[3])
+        return print(result.replace(" ", ""))
 
 
 ip = test_ip("132.15.11.0")
 ip.ip_to_bin(ip.split_ip())
-
-print('{:*^30}'.format('centered'))
+ip.__str__()
